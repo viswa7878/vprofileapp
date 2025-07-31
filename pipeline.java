2fab2109fce50efbb52eb8930bdd19c860ad990a
sathvika <sathvikakommana@gmail.com>
2025-07-31 07:46:37 +0530

    fd

---
+++ pipeline.java
@@ -0,0 +1,12 @@
+pipeline {
+    agent any
+  
+    stages {
+        stage('fetch code')
+         {
+            steps {
+                sh echo "Fetching code from repository"
+            }
+        }
+    }
+}
